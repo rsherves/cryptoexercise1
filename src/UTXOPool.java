9c99c65db849756a4e19740ab54ad118ffa5a5d9
Rafa Sousa Herves <rafael.sousa.herves@cern.ch>
2018-05-11 14:38:53 +0200

    Minor refactor.

--- src/UTXOPool.java
+++ src/UTXOPool.java
@@ -1,51 +1,48 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
 public class UTXOPool {
 
     /**
      * The current collection of UTXOs, with each one mapped to its corresponding transaction output
      */
-    private HashMap<UTXO, Transaction.Output> H;
+    private Map<UTXO, Transaction.Output> pool;
 
     /** Creates a new empty UTXOPool */
     public UTXOPool() {
-        H = new HashMap<UTXO, Transaction.Output>();
+        pool = new HashMap<UTXO, Transaction.Output>();
     }
 
     /** Creates a new UTXOPool that is a copy of {@code uPool} */
     public UTXOPool(UTXOPool uPool) {
-        H = new HashMap<UTXO, Transaction.Output>(uPool.H);
+        pool = new HashMap<UTXO, Transaction.Output>(uPool.pool);
     }
 
     /** Adds a mapping from UTXO {@code utxo} to transaction output @code{txOut} to the pool */
     public void addUTXO(UTXO utxo, Transaction.Output txOut) {
-        H.put(utxo, txOut);
+        pool.put(utxo, txOut);
     }
 
     /** Removes the UTXO {@code utxo} from the pool */
     public void removeUTXO(UTXO utxo) {
-        H.remove(utxo);
+        pool.remove(utxo);
     }
 
     /**
-     * @return the transaction output corresponding to UTXO {@code utxo}, or null if {@code utxo} is
-     *         not in the pool.
+     * @return the transaction output corresponding to UTXO {@code utxo}, or null if {@code utxo} is not in the pool.
      */
     public Transaction.Output getTxOutput(UTXO ut) {
-        return H.get(ut);
+        return pool.get(ut);
     }
 
     /** @return true if UTXO {@code utxo} is in the pool and false otherwise */
     public boolean contains(UTXO utxo) {
-        return H.containsKey(utxo);
+        return pool.containsKey(utxo);
     }
 
     /** Returns an {@code ArrayList} of all UTXOs in the pool */
-    public ArrayList<UTXO> getAllUTXO() {
-        Set<UTXO> setUTXO = H.keySet();
-        ArrayList<UTXO> allUTXO = new ArrayList<UTXO>();
+    public List<UTXO> getAllUTXO() {
+        Set<UTXO> setUTXO = pool.keySet();
+        List<UTXO> allUTXO = new ArrayList<UTXO>();
         for (UTXO ut : setUTXO) {
             allUTXO.add(ut);
         }
