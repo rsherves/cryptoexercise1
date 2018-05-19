a1e476d4669b943e7b2686d5ea2f79608761161d
Rafael Sousa Herves <rsherves@gmail.com>
2018-05-19 18:19:27 +0200

    Delete dummy Main.

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        TxHandler txHandler = new TxHandler(null);
-
-        System.out.println("Transaction Handler created, with empty UTXO pool");
-    }
-}
